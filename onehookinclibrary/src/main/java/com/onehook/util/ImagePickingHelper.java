a248e483a74ed7ef1a534402e9405d5ae1f3ae9c
Eagle Diao@Home <eaglediao@gmail.com>
2016-02-12 13:32:15 -0500

    adding new stuff

---
+++ onehookinclibrary/src/main/java/com/onehook/util/ImagePickingHelper.java
@@ -0,0 +1,56 @@
+package com.onehook.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by EagleDiao on 2016-02-10.
+ */
+public class ImagePickingHelper {
+
+    public static Intent createImagePickingIntent(final Context context, final File newImageFile, final int res) {
+        final Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        final Intent pickIntent = new Intent(Intent.ACTION_GET_CONTENT,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+        final Intent chooserIntent = Intent.createChooser(pickIntent, context.getString(res));
+        if (takePictureIntent.resolveActivity(context.getPackageManager()) != null) {
+            final File photoFile = newImageFile;
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{takePictureIntent});
+                return chooserIntent;
+            }
+        }
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{});
+        return pickIntent;
+    }
+
+    public static File createNewImageFile() {
+        final String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        final String imageFileName = "JPEG_" + timeStamp + "_";
+        final File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        try {
+            final File image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+            Log.d("OH ImagePickingHelper", "Create new temp file " + image.getAbsolutePath());
+            return image;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
---
+++ onehookinclibrary/src/main/java/com/onehook/view/NonSwipableViewPager.java
@@ -0,0 +1,30 @@
+package com.onehook.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by EagleDiao on 2016-02-08.
+ */
+public class NonSwipableViewPager extends ViewPager {
+
+    public NonSwipableViewPager(Context context) {
+        super(context);
+    }
+
+    public NonSwipableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }
+}
