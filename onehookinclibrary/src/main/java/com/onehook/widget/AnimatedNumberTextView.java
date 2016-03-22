0b358503541c80366fee790004eede1e87d722dc
Eagle Diao@Home <eaglediao@gmail.com>
2016-03-22 17:35:41 -0400

    animated number text view

---
+++ onehookinclibrary/src/main/java/com/onehook/widget/AnimatedNumberTextView.java
@@ -0,0 +1,55 @@
+package com.onehook.widget;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by EagleDiao on 2016-03-22.
+ */
+public class AnimatedNumberTextView extends TextView {
+
+    private int number;
+
+    public AnimatedNumberTextView(Context context) {
+        super(context);
+    }
+
+    public AnimatedNumberTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AnimatedNumberTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(21)
+    public AnimatedNumberTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void commonInit() {
+        setNumber(0);
+    }
+
+    /**
+     * For property animator.
+     *
+     * @param newNumber new number
+     */
+    public void setNumber(final int newNumber) {
+        this.number = newNumber;
+        setText(String.valueOf(newNumber));
+    }
+
+    public ValueAnimator createAnimation(final int fromNumber, final int toNumber, final long duration) {
+        return ObjectAnimator.ofInt(this, "number", fromNumber, toNumber).setDuration(duration);
+    }
+
+    public ValueAnimator createAnimation(final int toNumber, final long duration) {
+        return ObjectAnimator.ofInt(this, "number", this.number, toNumber).setDuration(duration);
+    }
+}
