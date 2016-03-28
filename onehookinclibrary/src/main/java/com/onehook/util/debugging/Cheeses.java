d6529da9635d7e69987f03cb79105e776649f081
Eagle Diao@Home <eaglediao@gmail.com>
2016-03-28 11:25:18 -0400

    progress dialog builder

--- onehookinclibrary/src/main/java/com/onehook/util/debugging/Cheeses.java
+++ onehookinclibrary/src/main/java/com/onehook/util/debugging/Cheeses.java
@@ -38,6 +38,10 @@ public class Cheeses {
         return sCheeseStrings[index];
     }
 
+    public static int getRandomNaturalNumberWithin(final int limit) {
+        return RANDOM.nextInt(limit);
+    }
+
     public static String getRandomDescription() {
         return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     }
--- onehookinclibrary/src/main/java/com/onehook/widget/loadingdialog/ProgressDialogFragment.java
+++ onehookinclibrary/src/main/java/com/onehook/widget/loadingdialog/ProgressDialogFragment.java
@@ -11,18 +11,74 @@ import android.support.v4.app.DialogFragment;
  */
 public class ProgressDialogFragment extends DialogFragment {
 
-    public static ProgressDialogFragment newInstance() {
-        return new ProgressDialogFragment();
+    private static final String KEY_TITLE = "keyTitle";
+
+    private static final String KEY_MESSAGE = "keyMessage";
+
+    private static final String KEY_CANCELABLE = "keyCancelable";
+
+    public static class ProgressDialogFragmentBuilder {
+
+        private String mTitle;
+
+        private String mMessage;
+
+        private boolean mCancelable;
+
+        public ProgressDialogFragmentBuilder() {
+            mTitle = null;
+            mMessage = null;
+            mCancelable = false;
+        }
+
+        public ProgressDialogFragmentBuilder setTitle(final String title) {
+            mTitle = title;
+            return this;
+        }
+
+        public ProgressDialogFragmentBuilder setMessage(final String message) {
+            mMessage = message;
+            return this;
+        }
+
+        public ProgressDialogFragmentBuilder setCancelable(final boolean cancelable) {
+            mCancelable = cancelable;
+            return this;
+        }
+
+        public ProgressDialogFragment build() {
+            final ProgressDialogFragment progressDialogFragment = new ProgressDialogFragment();
+            final Bundle args = new Bundle();
+            if (mTitle != null) {
+                args.putString(KEY_TITLE, mTitle);
+            }
+            if (mMessage != null) {
+                args.putString(KEY_MESSAGE, mMessage);
+            }
+            args.putBoolean(KEY_CANCELABLE, mCancelable);
+            progressDialogFragment.setArguments(args);
+            return progressDialogFragment;
+        }
     }
+
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Bundle args = getArguments();
+        final String title = args.getString(KEY_TITLE);
+        final String message = args.getString(KEY_MESSAGE);
+        final boolean cancelable = args.getBoolean(KEY_CANCELABLE);
+
         final ProgressDialog dialog = new ProgressDialog(getActivity());
-        dialog.setTitle("A MESSAGE");
-        dialog.setMessage("SOME MESSAGE");
+        if (title != null) {
+            dialog.setTitle(title);
+        }
+        if (message != null) {
+            dialog.setMessage(message);
+        }
         dialog.setIndeterminate(true);
-        dialog.setCancelable(false);
+        dialog.setCancelable(cancelable);
         return dialog;
     }
 }
